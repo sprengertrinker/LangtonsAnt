445a63d3f61d85b5918b38949841f06e253e3441
Lily <moch@uoregon.edu>
2016-03-11 13:46:20 -0800

    Working graphical prototype!

--- aunt.py
+++ aunt.py
@@ -1,59 +1,105 @@
 import math
-
+import time
+from tkinter import *
 __author__ = 'Mochla'
 
 
-class Langton:
+class Langton(Frame):
     gridSize = None
-    grid = None
-    currentX = None
-    currentY = None
+    cgrid = None
+    # Keeps track of the tkinter labels for color-changing purposes
+    tgrid = None
+    cx = None
+    cy = None
     # N E S W
     # 0 1 2 3
     # Initialize the facing direction to West (left) to start
     facing = 3
 
-    def __init__(self, size):
+    def __init__(self, parent, size):
+        # Tkinter parent frame init
+        Frame.__init__(self, parent, width=100, height=100, background="gray")
+        self.parent = parent
+        b = Button(parent, text="Run", command=self.run)
+        b.pack(anchor=CENTER, side=BOTTOM)
+
+        # Child frame init
+        self.gridFrame = Frame(parent)
+        self.gridFrame.pack(anchor=CENTER, side=TOP)
+
+        # Console output stuff
         self.gridSize = size
-        self.grid = [[0 for x in range(size)] for x in range(size)]
+        self.cgrid = [[0 for x in range(size)] for x in range(size)]
+        self.tgrid = [[0 for x in range(size)] for x in range(size)]
         mid = math.floor(size/2)
-        self.currentX = mid
-        self.currentY = mid
+        self.cx = mid
+        self.cy = mid
+
+        # Tkinter grid make
+        for i in range(size):
+            for j in range(size):
+                w = Label(self.gridFrame, bg="white", width="4", height="2", bd="2")
+                w.grid(row=i, column=j)
+                self.tgrid[i][j] = w
+
+        # Initial placement of the "ant"
+        self.tgrid[self.cx][self.cy]["bg"] = "green"
+        self.cgrid[self.cx][self.cy] = 1
+
+        self.centerwindow()
+        self.initui()
 
     def turn(self):
-        if self.grid[self.currentX][self.currentY] == 0:
-            self.grid[self.currentX][self.currentY] = 1
+        if self.cgrid[self.cx][self.cy] == 0:
+            self.cgrid[self.cx][self.cy] = 1
             self.facing = (self.facing + 1) % 4
         else:
-            self.grid[self.currentX][self.currentY] = 0
+            self.cgrid[self.cx][self.cy] = 0
             self.facing = (self.facing + 3) % 4
 
     def move(self):
+        if self.cgrid[self.cx][self.cy] == 0:
+            self.tgrid[self.cx][self.cy]["bg"] = "white"
+        if self.cgrid[self.cx][self.cy] == 1:
+            self.tgrid[self.cx][self.cy]["bg"] = "black"
+
+        # N - E - S - W
         if self.facing == 0:
-            self.currentY -= 1
+            self.cy -= 1
         elif self.facing == 1:
-            self.currentX += 1
+            self.cx += 1
         elif self.facing == 2:
-            self.currentY += 1
+            self.cy += 1
         elif self.facing == 3:
-            self.currentX -= 1
+            self.cx -= 1
         else:
             print("OMG FACING ERROR, facing = " + str(self.facing))
+            exit(1)
+        self.tgrid[self.cx][self.cy]["bg"] = "green"
 
-    def run(self, steps):
-        for i in range(steps):
-            for value in self.grid:
-                print(value)
-            self.move()
-            self.turn()
-            print("\n")
+    def run(self):
+        self.move()
+        self.turn()
 
-        for value in self.grid:
-            print(value)
+    def initui(self):
+        self.parent.title("Langton's Ant")
+
+    # Centers initial window on the screen
+    def centerwindow(self):
+        w = 400
+        h = 400
+
+        sw = self.parent.winfo_screenwidth()
+        sh = self.parent.winfo_screenheight()
+
+        x = (sw - w) / 2
+        y = (sh - h) / 2
+        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 
 def main():
-    app = Langton(5)
-    app.run(10)
+    root = Tk()
+    app = Langton(root, 10)
+    root.mainloop()
 
 main()
